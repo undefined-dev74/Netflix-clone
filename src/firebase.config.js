76bef6445887310a430a27e405ca379f9d51219a
JuniorBaymax <cooladitya953@gmail.com>
2021-04-22 23:55:54 +0530

    Added and Modified folder structure of the App

---
+++ src/firebase.config.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase';
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyAnsKtJ8KF2muXeqC5CrDTRECRaxnnTciY',
+  authDomain: 'netflix-clone-d5b65.firebaseapp.com',
+  projectId: 'netflix-clone-d5b65',
+  storageBucket: 'netflix-clone-d5b65.appspot.com',
+  messagingSenderId: '1042637457201',
+  appId: '1:1042637457201:web:1a46299954a86ed7e41210',
+  measurementId: 'G-JZGLP8TGXJ',
+};
