f48f454dcd154dc98edc266c27c97de86c73254e
JuniorBaymax <cooladitya953@gmail.com>
2021-04-24 23:57:39 +0530

    Setup TMDB API for te movie details

---
+++ src/API/Request.js
@@ -0,0 +1,15 @@
+const API_KEY = '9a5fa99784bef049e33567f5a6309012';
+
+const requests = {
+  fetchTranding: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+};
+
+export default requests;
+// ? DemoAPI https://api.themoviedb.org/3/discover/movie?api_key=9a5fa99784bef049e33567f5a630901&with_genres=99
