43895241a8859a37140df31958c930f2a2497dfb
JuniorBaymax <cooladitya953@gmail.com>
2021-04-26 01:33:46 +0530

    Added Pages/Login to the app

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import './Login.css';
+const Login = () => {
+  const [signIn, setSignIn] = useState(false);
+  return (
+    <div className="login">
+      <div className="login_background">
+        <img
+          className="login_logo"
+          src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
+          alt="login_picture"
+        />
+        <button onClick={() => setSignIn(true)} className="login_button">
+          Sign In
+        </button>
+        <div className="login_gradient" />
+      </div>
+      <div className="login__body">
+        (signIn ? (
+        <SignIn />
+        ): (
+        <>
+          <h1>Unlimited films, TV programmes and more.</h1>
+          <h2>Watch anywhere, Cancel at any time.</h2>
+          <p>
+            Ready to watch? Enter your email to create or restart your
+            membership
+          </p>
+          <div className="login__input">
+            <form>
+              <input type="email" placeholder="Email Address" />
+              <button
+                onClick={() => setSignIn(true)}
+                className="login__getStarted"
+              >
+                GET STARTED
+              </button>
+            </form>
+          </div>
+        </>
+        ))
+      </div>
+    </div>
+  );
+};
+
+export default Login;
