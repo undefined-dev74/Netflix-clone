af38b5ae1dd690e3548db2cce6e41f545d229393
JuniorBaymax <cooladitya953@gmail.com>
2021-04-25 23:49:18 +0530

    Added React-Router-Dom dependencies to the App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import './App.css';
 import HomeScreen from '../src/containers/HomeScreen/HomeScreen';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 function App() {
   return (
     <div className="app">
-      <HomeScreen />
+      <Router>
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route exact path="/">
+            <HomeScreen />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
