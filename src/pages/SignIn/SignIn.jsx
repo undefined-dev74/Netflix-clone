6f7573224e89d1a4b4bb610852e61ad90345d037
JuniorBaymax <cooladitya953@gmail.com>
2021-04-26 22:32:49 +0530

    Login and SignIn page UI interface completed EOD the Commit of #4cf2a5438ac0ae7ba9aa1674c94a421ca0fb3d08

--- src/pages/SignIn/SignIn.jsx
+++ src/pages/SignIn/SignIn.jsx
@@ -1,13 +1,29 @@
 import React from 'react';
 import './SignIn.css';
+
 const SignIn = () => {
+  const register = e => {
+    e.preventDefault();
+  };
+
+  const signIn = e => {
+    e.preventDefault();
+  };
   return (
     <div className="signIn">
       <form>
         <h1>Sign In</h1>
         <input type="Email" placeholder="Email" />
         <input placeholder="Password" type="Password" />
-        <button type="submit">Sign In</button>
+        <button type="submit" onClick={signIn}>
+          Sign In
+        </button>
+        <h4>
+          <span className="signIn__gray">New to Netflix?</span>
+          <span className="signIn__link" onClick={register}>
+            Sign up now
+          </span>
+        </h4>
       </form>
     </div>
   );
