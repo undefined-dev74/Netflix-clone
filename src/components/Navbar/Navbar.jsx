53aa9d0c6f99af32b8c95f2f64a415d81c797feb
JuniorBaymax <cooladitya953@gmail.com>
2021-04-28 23:56:00 +0530

    Added Avatar icon on Navbar, component changed to flex with overall App

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router';
 import './Navbar.css';
 const Navbar = () => {
   const [show, handleShow] = useState(true);
+  const history = useHistory(); // ? React-Router Hooks
 
   const transitionNavbar = () => {
     if (window.scrollY > 100) {
@@ -23,11 +25,13 @@ const Navbar = () => {
     <div className={`navbar ${show && 'navbar_black'}`}>
       <div className="navbar_content">
         <img
+          onClick={() => history.push('/')}
           className="navbar_logo"
           src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
           alt=""
         />
         <img
+          onClick={() => history.push('/profile')}
           className="navbar_avatar"
           src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png"
           alt=""
