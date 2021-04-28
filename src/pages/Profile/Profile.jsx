40407e0a2b820065ca7751ed133ce194d6a5d398
JuniorBaymax <cooladitya953@gmail.com>
2021-04-28 23:53:10 +0530

    Customized and added  Profile Page to the App

---
+++ src/pages/Profile/Profile.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import './Profile.css';
+import { useSelector } from 'react-redux';
+import Navbar from '../../components/Navbar/Navbar';
+import { selectUser } from '../../features/userSlice';
+import { auth } from '../../firebase.config';
+
+const Profile = () => {
+  const user = useSelector(selectUser);
+
+  return (
+    <div className="profile">
+      <Navbar />
+      <div className="profile__body">
+        <h1>Edit Profile</h1>
+        <div className="profile__info">
+          <img
+            src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png"
+            alt="profile_avatar"
+          />
+          <div className="profile__details">
+            <h2>{user.email}</h2>
+            <div className="profile__plans">
+              <h3>Plans</h3>
+            </div>
+            <button onClick={() => auth.signOut()} className="profile__signOut">
+              Sign Out
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
